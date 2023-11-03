3c1ba6802695865a239f64d5bc17ecbb4e6855a0
Andrew Neal <andrewneal@gmx.com>
2023-11-03 03:34:58 -0400

    Implemented working fuzzy string comparison.

--- include/fuzzymatching.h
+++ include/fuzzymatching.h
@@ -2,6 +2,17 @@
 #ifndef FUZZYMATCHING_H
 #define FUZZYMATCHING_H
 
+// Takes pointers to two strings to be compared, and returns an integer "score"
+// representing how similar the two strings are. The score will be normalized
+// to a nominal 0-100. 0 is no similarity, and 100+ is high similarity. A Full
+// match is never truly determined, and scores greater than 100 can be returned
+// from regularly repeating strings. This will be left as such, as it would add
+// complexity and CPU time to each comparison, and it matters very little in
+// the real world where repeating strings are uncommon. Not to mention that
+// return values > 100 are still interpreted as high similarity. Not perfect
+// but good enough.
+float fuzzycmp(char *str1, char *str2);
+
 // Takes a pointer to a string to search for, the string to search for it in,
 // a score threshold to throttle unlikely matches, and returns a pointer to an
 // allocated, '\n' delimited string, sorted with the higher scoring matches on
--- src/fuzzymatching.c
+++ src/fuzzymatching.c
@@ -1,4 +1,5 @@
 #include <stddef.h>
+#include <stdio.h>
 
 // Maximum size of comparison
 #define CMP_B_SIZE 512
@@ -7,6 +8,12 @@ typedef enum {
 	NEEDLE,
 	HAYSTACK
 } n_or_h;
+
+typedef struct {
+	char *result;
+	int score;
+	void *next;
+} fuzzylist;
 
 // Function for measuring the length of a string at a specified delimiter
 // rather than a null byte only, but still stops on a null byte
@@ -22,16 +29,62 @@ static int dstrlen(char *str, char delim) {
 // char array, stopping at delim, null, or size.
 static void dstrncat(char *dst, char *src, size_t size, char delim) {
 	int startat = dstrlen(dst, '\0');
-	int endat = startat+size-1;
+	char *dst_offset = &dst[startat];
+	//int endat = startat+size-1;
 	for(size_t i=0; i<size-1; i++) {
-		if(src[i] != delim && src[i] != '\0') {
-			dst[i+startat] = src[i];
+		if(src[i] == delim || src[i] == '\0') {
+			dst_offset[i] = '\0';
+			break;
 		}
+		dst_offset[i] = src[i];
 	}
 	return;
 }
 
+static void to_lower(char *str) {
+	int len = dstrlen(str, '\n');
+	for(int i=0; i<len; i++) {
+		if(str[i] >= 'A' && str[i] <= 'Z') str[i] ^= 0b00100000;
+	}
+	return;
+}
+
+int fuzzycmp(char *str1, char *str2) {
+	int score = 0;
+	int len1 = dstrlen(str1, '\n');
+	int len2 = dstrlen(str2, '\n');
+	if(len1 == 0 || len2 == 0) return 0;
+	char *longer = str1;
+	int llen = len1;
+	char *shorter = str2;
+	int slen = len2;
+	if(len1 <= len2) {
+		longer = str2;
+		shorter = str1;
+		llen = len2;
+		slen = len1;
+	}
+	int iter = llen;
+	for(int i=0; i<iter; i++) {
+		int scoreswitch = 0;
+		for(int j=0; j<slen; j++) {
+			if(!(shorter[j] ^ longer[(i+j)%llen])) {
+				if(scoreswitch > 0) {
+					if(scoreswitch == 1) {
+						score += 2;
+						scoreswitch = 2;
+					} else score++;
+				} else scoreswitch = 1;
+			} else scoreswitch = 0;
+		}
+	}
+	printf("Unweighted score: %d\n", score);			//debug
+	score = (float)score * (float)slen / (float)llen / (float)llen * 100;
+	return score;
+}
+
 char *fuzzymatch(char *needle, char *haystack, int threshold) {
+	/*
 	int nlen = dstrlen(needle, '\0');
 	int hlen = dstrlen(haystack, '\n');
 	if(!nlen || !hlen) return NULL;
@@ -41,23 +94,23 @@ char *fuzzymatch(char *needle, char *haystack, int threshold) {
 	size_t hbufsize = 0;
 	char nbuf[CMP_B_SIZE*2] = { '\0' };
 	char hbuf[CMP_B_SIZE*2] = { '\0' };
-	char *copy_once = &hbuf;
+	char *copy_once = hbuf;
 	char *copy_once_src = haystack;
-	char *copy_more = &nbuf;
+	char *copy_more = nbuf;
 	char *copy_more_src = needle;
 	if(nlen >= hlen) {
 		iter = hlen;
 		bigger = NEEDLE;
-		char *copy_once = &nbuf;
+		char *copy_once = nbuf;
 		char *copy_once_src = needle;
-		char *copy_more = &hbuf;
+		char *copy_more = hbuf;
 		char *copy_more_src = haystack;
 	}
 	int copy_times;
 	if(bigger == NEEDLE) {
 		copy_times = nlen / hlen;
 	} else copy_times = hlen / nlen;
-
+	*/
 
 	return NULL;
 }
